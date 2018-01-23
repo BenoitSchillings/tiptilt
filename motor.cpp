3327904104e8fa3559e92cb88a0d2a21f76f5ebf
Benoit Schillings <bschillings@bschillings-macbookpro.roam.corp.google.com>
2018-01-23 14:26:04 -0800

    ping

---
+++ motor.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+//--------------------------------------------------------------------
+
+
+
+class	tiptilt {
+public:;
+
+		tiptilt();
+		~tiptilt();
+
+	void	Move(float d_alpha, float d_beta);
+	void	MoveTo(float alpha, float beta);	
+	void	Reset();
+
+	void	MoveFocus(float delta);
+	void	MoveToFocus(float value);	
+
+private:
+	int	InitUSB();
+	void	CloseUSB();
+
+	int	WriteCommand(string cmd);
+	string	ReadResult();
+};
+
+
+
+//--------------------------------------------------------------------
+
+	tiptilt::tiptilt()
+{
+}
+
+
+//--------------------------------------------------------------------
+
+
+	tiptilt::~tiptilt()
+{
+}
+
+//--------------------------------------------------------------------
+
+void	tiptilt::Move(float delta_alpha, float delta_beta)
+{
+}
+
+//--------------------------------------------------------------------
+
+void	tiptilt::MoveTo(float alpha, float beta)
+{
+}
+
+
+//--------------------------------------------------------------------
+
+
+void	tiptilt::MoveFocus(float delta)
+{
+}
+
+//--------------------------------------------------------------------
+
+void	tiptilt::MoveToFocus(float value)
+{
+}
+
+//--------------------------------------------------------------------
+
+int	tiptilt::InitUSB()
+{
+	return 0;
+}
+
+//--------------------------------------------------------------------
+
+void	tiptilt::CloseUSB()
+{
+}
+
+//--------------------------------------------------------------------
+
+int	tiptilt::WriteCommand(string cmd)
+{
+	return 0;
+}
+
+
+//--------------------------------------------------------------------
+
+string	tiptilt::ReadResult()
+{
+	return "";
+}
+
+//--------------------------------------------------------------------
+
+
+int main()
+{
+	return 0;
+}
